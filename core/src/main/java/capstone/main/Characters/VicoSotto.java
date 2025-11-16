8a8da2302751b0ddee4bdd9a90a2dcb029f73d0f
Murgwell <romar.alaman@cit.edu>
2025-11-16 13:49:25 +0800

    removed redundant files + more abstraction

--- core/src/main/java/capstone/main/Characters/VicoSotto.java
+++ core/src/main/java/capstone/main/Characters/VicoSotto.java
@@ -1,6 +1,5 @@
 package capstone.main.Characters;
 
-import capstone.main.Managers.DirectionManager;
 import com.badlogic.gdx.graphics.Texture;
 import capstone.main.Sprites.Bullet;
 
@@ -8,19 +7,12 @@ import java.util.ArrayList;
 
 public class VicoSotto extends AbstractPlayer implements Ranged {
 
-    private float health;
-    private float mana;
-    private float damage;
     ArrayList<Bullet> bullets;
-    private static float AttackSpeed = 100f; // Attack Speed
-    DirectionManager directionManager;
 
-
-    public VicoSotto(float x, float y, float width, float height,
+    public VicoSotto(float healthPoints, float manaPoints, float baseDamage, float maxDamage, float attackSpeed,float x, float y, float width, float height,
                      ArrayList<Bullet> bullets, float worldWidth, float worldHeight) {
-        super(120, 80, AttackSpeed, new Texture("character.png"), x, y, width, height, worldWidth, worldHeight);
+        super(healthPoints, manaPoints, baseDamage, maxDamage, attackSpeed, new Texture("character.png"), x, y, width, height, worldWidth, worldHeight);
         this.bullets = bullets;
-        directionManager = new DirectionManager(sprite);
     }
 
     public void performAttack(float delta, float weaponRotationRad) {
