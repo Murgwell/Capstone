ddbb1921f7f90d24a9c3868eed50a5872c2f1c2b
Cedibyte <CeddieCastro@outlook.com>
2025-12-02 03:22:37 +0800

    Revert "implemented screenshake"
    This reverts commit 0cf17664

--- core/src/main/java/capstone/main/Characters/VicoSotto.java
+++ core/src/main/java/capstone/main/Characters/VicoSotto.java
@@ -1,21 +1,36 @@
 package capstone.main.Characters;
 
 import capstone.main.Managers.ScreenShake;
-import com.badlogic.gdx.graphics.Texture;
 import capstone.main.Sprites.Bullet;
 import com.badlogic.gdx.physics.box2d.World;
 
 import java.util.ArrayList;
 
 public class VicoSotto extends AbstractPlayer implements Ranged {
-    ArrayList<Bullet> bullets;
+
+    private final ArrayList<Bullet> bullets;
     private final ScreenShake screenShake;
 
     public VicoSotto(float healthPoints, float manaPoints, float baseDamage, float maxDamage,
                      float attackSpeed, float x, float y, float width, float height,
-                     ArrayList<Bullet> bullets, float worldWidth, float worldHeight, World physicsWorld, ScreenShake screenShake) {
-        super(healthPoints, manaPoints, baseDamage, maxDamage, attackSpeed,
-            new Texture("character.png"), x, y, width, height, worldWidth, worldHeight, physicsWorld);
+                     ArrayList<Bullet> bullets, float worldWidth, float worldHeight,
+                     World physicsWorld, ScreenShake screenShake) {
+        super(
+            healthPoints,
+            manaPoints,
+            baseDamage,
+            maxDamage,
+            attackSpeed,
+            "Vico Sotto", // Use character name for animations
+            x,
+            y,
+            width,
+            height,
+            worldWidth,
+            worldHeight,
+            physicsWorld
+        );
+
         this.bullets = bullets;
         this.screenShake = screenShake;
     }
@@ -24,13 +39,13 @@ public class VicoSotto extends AbstractPlayer implements Ranged {
     public void performAttack(float delta, float weaponRotationRad) {
         if (!canAttack()) return;
         onAttackPerformed();
-        // Spawn bullet logic can be added here
+        // TODO: Add Vico-specific attack behavior (bullets, effects, etc.)
     }
 
     @Override
     protected void onDamaged(float delta) {
         super.onDamaged(delta);
-        screenShake.shake(0.20f,0.2f);
+        screenShake.shake(0.20f, 0.2f);
     }
 
     @Override
